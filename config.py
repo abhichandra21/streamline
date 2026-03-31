9abd1d73b4830a90009c8c1a3946d514a18d9c7e
abhichandra21 <abhi.chandra@gmail.com>
2026-03-31 00:44:39 -0500

    chore: project scaffolding for recommendation system

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+TMDB_API_KEY = os.environ.get("TMDB_API_KEY", "55a3ba0fd07d2cd884871536b48dd04f")
+
+PLATFORM_PATHS = {
+    "netflix": "netflix/2468198246996321264/CONTENT_INTERACTION/ViewingActivity.csv",
+    "prime": None,    # set when data arrives
+    "disney": None,
+    "hbo": None,
+}
+
+DEFAULT_TOP_N = 10
+CANDIDATE_POOL_SIZE = 500
+MIN_VOTE_COUNT = 100
+RECENCY_HALF_LIFE_DAYS = 90
+CACHE_DIR = "recommender/cache/tmdb"
